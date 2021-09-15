c9b5bd5a5d5373e835f4639cb332894bd191f340
DESKTOP-7RIDP6K\c_bri <catalin.briscan@principal33.com>
2021-09-16 02:09:13 +0300

    Tried tu vue the addition to the table

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,6 +1,28 @@
 <template>
     <div style="overflow-x: auto;">
         <table id="myTable">
+            <!-- <thead>
+                <tr>
+                    <th v-for="header in headers" :key="header">
+                        {{header}}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-if="items.length === 0">
+                    <td>No data</td>
+                </tr>
+                <tr v-else v-for="item in items" :key="item.no">
+                    <td>{{item.no}}</td>
+                    <td>{{item.lastname}}</td>
+                    <td>{{item.firstname}}</td>
+                    <td>{{item.email}}</td>
+                    <td>{{item.gender}}</td>
+                    <td>{{item.birthday}}</td>
+                    <td>{{item.image}}</td>
+                </tr>
+            </tbody> -->
+
             <th id="tlname">Last Name</th>
             <th id="tfname">First Name</th>
             <th id="temail">Email</th>
@@ -12,5 +34,23 @@
         </table>
     </div>
 </template>
+
+// <script>
+// var overall = new Vue({
+//     el: '#myTable',
+//     data: {
+//       headers: [
+//         "no", "lastname", "firstname", "email", "gender", "birthday", "image"
+//       ],
+//       items: []
+//     },
+//     methods: {
+//       add: function(item) {
+//         this.items.push(item);// = item;
+//       }
+//     }
+//   });
+// </script>
+
 
 <style scoped src="../styles/table.css"></style>
